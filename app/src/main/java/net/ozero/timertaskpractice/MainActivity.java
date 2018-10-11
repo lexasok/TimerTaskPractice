17cc1e1ea49192215894251407548c4c1c1ee821
alex <sokolov@ozero.net>
2018-10-12 01:57:59 +0300

    Service initialized

--- app/src/main/java/net/ozero/timertaskpractice/MainActivity.java
+++ app/src/main/java/net/ozero/timertaskpractice/MainActivity.java
@@ -1,15 +1,12 @@
 package net.ozero.timertaskpractice;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.support.v4.app.NotificationCompat;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import net.ozero.timertaskpractice.services.TimerService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,39 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(getClass().getName(), "onCreate");
     }
 
     public void buttonAddClicked(View view) {
 
-        Timer timer = new Timer();
-        NotificationTask task = new NotificationTask();
-        timer.schedule(
-                task,
-                new Date(System.currentTimeMillis() + 10*1000)
-        );
-    }
-
-    class NotificationTask extends TimerTask {
-
-        @Override
-        public void run() {
-            sendNotification();
-        }
-    }
-
-    private void sendNotification() {
-
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Title")
-                        .setContentText("Notification text");
-
-        Notification notification = builder.build();
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        assert notificationManager != null;
-        notificationManager.notify(1, notification);
+        Intent intent = new Intent(this, TimerService.class);
+        startService(intent);
     }
 }
---
+++ app/src/main/java/net/ozero/timertaskpractice/services/TimerService.java
@@ -0,0 +1,59 @@
+package net.ozero.timertaskpractice.services;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import net.ozero.timertaskpractice.R;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerService extends IntentService {
+
+    public TimerService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                sendNotification();
+            }
+        };
+        timer.schedule(
+                task,
+                new Date(System.currentTimeMillis() + 10*1000)
+        );
+
+        Log.i(getClass().getName(), "task set");
+    }
+
+    private void sendNotification() {
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Title")
+                        .setContentText("Notification text");
+
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        assert notificationManager != null;
+        notificationManager.notify(1, notification);
+
+        Log.i(getClass().getName(), "notification sen");
+    }
+}
